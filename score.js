1742451f5bf9e2571b2ca0cd73407e47787c57f4
Che-Huai Lin <lzh9102@gmail.com>
2018-05-21 14:24:49 +0800

    Refactor into functions

--- score.js
+++ score.js
@@ -7,68 +7,95 @@ $(document).ready(function() {
         .add('f_clef', 'assets/img/f_clef_240px.png')
         .add('whole_note', 'assets/img/whole_note.png')
   loader.load(function(loader, res) {
+    const LINE_TOP = 52;
+    const LINE_SPACING = 31
 
-    let line_top = 52;
-    let line_spacing = 31;
-    let line_positions = [];
-    for (let i = 0; i < 5; i++)
-      line_positions.push(line_top + line_spacing * i);
+    function createStaffLines(width) {
+      let lines = new PIXI.Graphics();
+      for (let i = 0; i < 5; i++) {
+        let pos = LINE_TOP + LINE_SPACING * i;
+        lines.lineStyle(3, 0x00000000);
+        lines.moveTo(0, pos);
+        lines.lineTo(width, pos);
+      }
+      return lines;
+    }
 
-    // treble clef
-    let treble_clef = new PIXI.Container();
+    // position: the middle line is 0; upward is positive; downward is negative
+    // example: middle C in treble clef -> createNote(-6)
+    function createNote(position) {
+      function positionToY(position) {
+        return LINE_TOP + 2 * LINE_SPACING - position * (LINE_SPACING/2);
+      }
+
+      function createLedgerLines(width, position) {
+        if (position <= -6 || position >= 6) {
+          // ledger lines are drawn at +6, -6, +8, -8, +10, -10, ...
+          let begin = (position > 0) ? 6 : -6;
+          let incr = (position > 0) ? 2 : -2;
+          let count = Math.floor(Math.abs(position - begin) / 2) + 1;
+          let ledger_lines = new PIXI.Graphics();
+          for (let i = 0; i < count; i++) {
+            let ledger_pos = begin + incr * i;
+            let ledger_y = positionToY(ledger_pos);
+            ledger_lines.lineStyle(3, 0x00000000);
+            ledger_lines.moveTo(-10, ledger_y);
+            ledger_lines.lineTo(width + 10, ledger_y);
+          }
+          return ledger_lines;
+        }
+        return null;
+      }
+
+      let note = new PIXI.Container();
+      note.y = positionToY(position);
+
+      let note_body = new PIXI.Sprite(res.whole_note.texture);
+      note_body.y = -note_body.height / 2; // y-center the note
+      note.addChild(note_body);
+
+      let ledger_lines = createLedgerLines(note.width, position);
+      if (ledger_lines) {
+        ledger_lines.y = -note.y; // position ledger lines relative to note
+        note.addChild(ledger_lines);
+      }
+
+      return note;
+    }
+
+    function createTrebleClef(width) {
+      let treble_clef = new PIXI.Container();
+      treble_clef.addChild(new PIXI.Sprite(res.g_clef.texture)); // G-clef
+      treble_clef.addChild(createStaffLines(width));
+      return treble_clef;
+    }
+
+    function createBassClef(width) {
+      let bass_clef = new PIXI.Container();
+      bass_clef.addChild(new PIXI.Sprite(res.f_clef.texture)); // F-clef
+      bass_clef.addChild(createStaffLines(width));
+      return bass_clef;
+    }
+
+    let treble_clef = createTrebleClef(700);
     treble_clef.x = 30;
     treble_clef.y = 30;
     app.stage.addChild(treble_clef);
 
-    let g_clef = new PIXI.Sprite(res.g_clef.texture);
-    treble_clef.addChild(g_clef);
-
-    let treble_lines = new PIXI.Graphics();
-    for (let y of line_positions) {
-      treble_lines.lineStyle(3, 0x00000000);
-      treble_lines.moveTo(0, y);
-      treble_lines.lineTo(700, y);
-    }
-    treble_clef.addChild(treble_lines);
-
-    // bass clef
-    let bass_clef = new PIXI.Container();
+    let bass_clef = createBassClef(700);
     bass_clef.x = 30;
     bass_clef.y = 300;
     app.stage.addChild(bass_clef);
 
-    let f_clef = new PIXI.Sprite(res.f_clef.texture);
-    bass_clef.addChild(f_clef);
+    // Bass Clef: C
+    let note1 = createNote(-1);
+    note1.x = 100;
+    bass_clef.addChild(note1);
 
-    let bass_lines = new PIXI.Graphics();
-    for (let y of line_positions) {
-      bass_lines.lineStyle(3, 0x00000000);
-      bass_lines.moveTo(0, y);
-      bass_lines.lineTo(700, y);
-    }
-    bass_clef.addChild(bass_lines);
-
-    // simple note
-    let note = new PIXI.Sprite(res.whole_note.texture);
-    note.x = 100
-    note.y = line_positions[2];
-    bass_clef.addChild(note);
-
-    // note with ledger lines
-    let note2 = new PIXI.Container();
+    // Treble Clef: C
+    let note2 = createNote(-6);
     note2.x = 100;
-    note2.y = line_positions[4] + line_spacing / 2;
     treble_clef.addChild(note2);
-
-    let note2_note = new PIXI.Sprite(res.whole_note.texture);
-    note2.addChild(note2_note);
-
-    let note2_ledger_lines = new PIXI.Graphics();
-    note2_ledger_lines.lineStyle(3, 0x00000000);
-    note2_ledger_lines.moveTo(-10, line_spacing / 2);
-    note2_ledger_lines.lineTo(note2.width + 10, line_spacing / 2);
-    note2.addChild(note2_ledger_lines);
-
   });
 
 });
